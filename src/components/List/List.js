8fc2210de88fbd77598db4c9b0b8ac7b815facc9
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-05-07 22:54:46 +0200

    List and column refactoring

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import PropTypes from 'prop-types';
-// import Column from '../Column/Column.js';
+import Column from '../Column/ColumnContainer.js';
 import { settings } from '../../data/dataStore.js';
 import ReactHtmlParser from 'react-html-parser';
 // import Creator from '../Creator/Creator';
@@ -20,19 +20,19 @@ class List extends React.Component {
   };
 
   render() {
-    const {title, image, description} = this.props;
+    const {title, image, description, columns} = this.props;
     return (
       <section className={styles.component}>
         <Hero titleText={title} imageLink={image} />
         <div className={description}>
           {ReactHtmlParser(description)}
         </div>
-        {/* <div className={styles.columns}>
-          {this.state.columns.map(({ key, ...columnProps }) => (
-            <Column key={key} {...columnProps} />
+        <div className={styles.columns}>
+          {columns.map(columnData => (
+            <Column key={columnData.id} {...columnData} />
           ))}
         </div>
-        <div className={styles.creator}>
+        {/* <div className={styles.creator}>
           <Creator
             text={settings.columnCreatorText}
             action={(title) => this.addColumn(title)}
