cfca98cf9dbed3f06e48f314955560b11a32c2c6
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-05-16 18:59:10 +0200

    Add react-router-transition

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Home from '../Home/HomeContainer.js';
 import Info from '../Info/Info.js';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout.js';
 import FAQ from '../FAQ/FAQ.js';
+import {AnimatedSwitch} from 'react-router-transition';
+import styles from './App.scss';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
+      <AnimatedSwitch
+        atEnter={{ opacity: 0 }}
+        atLeave={{ opacity: 0 }}
+        atActive={{ opacity: 1 }}
+        className={styles.switchWrapper}>
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={FAQ} />
-      </Switch>
+      </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
